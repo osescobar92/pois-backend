2938e37b80120fc58809091869865792ed94414b
Oscar Escobar Ulloa <osescobar92@gmail.com>
2023-12-26 22:58:14 -0300

    create the routes for responses 404 and 500

--- index.js
+++ index.js
@@ -4,9 +4,18 @@ const app = express()
 const routes = require('./routes/routes');
 
 app.get('/', (req, res) => {
-    res.send('Hello world');
+    res.send('Bienvenido a la prueba Puntos de Interés para postular a la empresa XBrein');
 });
 app.use('/pois', routes);
+
+app.use(function (req, res) {
+    res.status(404).send('Sorry cant find the page!')
+})
+
+app.use(function (err, req, res) {
+    console.error(err.stack)
+    res.status(500).send('Something broke!')
+})
 
 app.listen(3000, () => {
     console.log('La app está escuchando en el puerto 3000')
